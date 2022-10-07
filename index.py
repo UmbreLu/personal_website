2272774b6f96c8afd7eeead0dfb514a2a3104565
Lucas <lucaspinto90@hotmail.com>
2022-10-07 09:56:43 -0300

    removed unfunctional analytics due to non persistent host environment

--- index.py
+++ index.py
@@ -1,17 +1,12 @@
-from flask import Flask, render_template, request
-from my_tools import Analytics 
+from flask import Flask, render_template
 
 app = Flask(__name__)
-stats = Analytics('analytics.db')
 
 @app.get('/')
 def greeting():
     """
     Index page.
     """
-    stats.analyse(request)
-    print(type(request.endpoint))
-    print(type(request.user_agent))
     return render_template('greeting.html')
 
 @app.get('/home')
@@ -19,7 +14,6 @@ def home():
     """
     Main page.
     """
-    stats.analyse(request)
     return render_template('home.html')
 
 @app.get('/bio')
@@ -27,7 +21,6 @@ def bio():
     """
     About myself page.
     """
-    stats.analyse(request)
     return render_template('bio.html')
 
 @app.get('/contato.html')
@@ -35,7 +28,6 @@ def contato():
     """
     This is the contact page.
     """
-    stats.analyse(request)
     return render_template('contato.html')
 
 @app.get('/jobs')
@@ -43,7 +35,6 @@ def jobs():
     """
     Portfolio page.
     """
-    stats.analyse(request)
     return render_template('jobs.html')
 
 @app.get('/resume')
@@ -51,29 +42,7 @@ def resume():
     """
     Displays resume page.
     """
-    stats.analyse(request)
     return render_template('resume.html')
-
-@app.get('/analytics')
-def analytics():
-    """
-    return analytics
-    """
-    return stats.analytics()
-
-@app.get('/current')
-def current():
-    """
-    returns todays' analytical records
-    """
-    return stats.current()
-
-@app.get('/errorlog')
-def errorlog():
-    """
-    returns the error log records
-    """
-    return stats.error_log()
 
 
 if __name__ == "__main__":
--- setup.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE daily (day TEXT, unique_agents INTEGER, greeting INTEGER, home INTEGER, bio INTEGER, contato INTEGER, jobs INTEGER, resume INTEGER);
